03fe436dc6f18fa26b8e6e03664ae167e9066b7d
Alexis RIGOULOT <alexis.rigoulot@2bsystem.fr>
2022-12-13 10:46:59 +0100

    first commit

---
+++ start/index.js
@@ -0,0 +1,14 @@
+// module natif
+    const fs = require('fs');
+    // fonction native de node qui copie colle d'un fichier à un autre
+    fs.copyFile('html/src.html', 'html/dest.html', (error)=>{
+        if(error) throw error;
+        console.log("Copie réalisée avec succès !!");
+    });
+
+
+// module personnel
+    const libHello = require('./module/lib-hello');
+    libHello.helloWithName("Alexis");
+
+// module externe
---
+++ start/module/lib-hello.js
@@ -0,0 +1,9 @@
+"use strict";
+
+exports.hello = function hello (){
+    console.log('hello les copains !!!!');
+}
+
+exports.helloWithName =  function helloWithName (param){
+    console.log('hello ' + param);
+}
